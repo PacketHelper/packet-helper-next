73527936c80fd3801f28367933a6049ed4e5d0fb
m1k53r <mikobw369@protonmail.com>
2021-07-08 16:35:18 +0200

    Add support for more protocols, handle some annoying cases and use prettifier on html

--- src/components/Display.vue
+++ src/components/Display.vue
@@ -1,62 +1,58 @@
-<template>
-  <div class="wrapper">
-    <div v-if="data">
-      <v-card>
-        <DropDown>
-          <template v-slot:title>
-            <v-card-title>{{ data.name }}</v-card-title>
-            <v-card-subtitle>{{ data.tshark_name }}</v-card-subtitle>
-          </template>
-
-          <template v-slot:content>
-            <code>
-              <v-card-text>
-                <ul>
-                  <li>Packet length: {{ data.length }}{{ data.length_unit }}</li>
-                  <DropDown>
-                    <template v-slot:title>
-                      <li>Scapy code representation:</li>
-                    </template>
-
-                    <template v-slot:content>
-                      <ul>
-                        <li> <code>{{ data.repr }}</code></li>
-                      </ul>
-                    </template>
-                  </DropDown>
-                  <div class="raw-data" v-for="(tshark, key) in sortedData" :key="key">
-                    <div v-if="tshark.children">
-                      <DropDown>
-                        <template v-slot:title>
-                          <li class="collapse">{{ tshark.name }}: {{ tshark.value }}</li>
-                        </template>
-
-                        <template v-slot:content v-if="tshark.children">
-                          <ul>
-                            <li v-for="(child, key) in tshark.children" :key="key">
-                              <code>{{ child.name }}: {{ child.value }}</code>
-                            </li>
-                          </ul>
-                        </template>
-                      </DropDown>
-                    </div>
-
-                    <div v-else>
-                      <li>{{ tshark.name }}: {{ tshark.value }}</li>
-                    </div>
-                  </div>
-                </ul>
-              </v-card-text>
-            </code>
-          </template>
-        </DropDown>
-      </v-card>
-    </div>
-
-    <div v-else>
-      There was an error while loading data
-    </div>
-  </div>
+<template> 
+  <div class="wrapper"> 
+  <div v-if="data"> 
+    <v-card> 
+    <DropDown> 
+      <template v-slot:title> 
+      <v-card-title>
+        {{ data.name }}
+      </v-card-title> 
+      <v-card-subtitle>
+        {{ data.tshark_name }}
+      </v-card-subtitle> 
+      </template> 
+      <template v-slot:content> 
+      <code> 
+        <v-card-text> 
+        <ul> 
+          <li>Packet length: {{ data.length }}{{ data.length_unit }}</li> 
+          <DropDown> 
+          <template v-slot:title> 
+            <li>Scapy code representation:</li> 
+          </template> 
+          <template v-slot:content> 
+            <ul> 
+            <li> <code>{{ data.repr }}</code></li> 
+            </ul> 
+          </template> 
+          </DropDown> 
+          <div class="raw-data" v-for="(tshark, key) in sortedData" :key="key"> 
+          <div v-if="tshark.children"> 
+            <DropDown> 
+            <template v-slot:title> 
+              <li class="collapse">{{ tshark.name }}: {{ tshark.value }}</li> 
+            </template> 
+            <template v-slot:content v-if="tshark.children"> 
+              <ul> 
+              <li v-for="(child, key) in tshark.children" :key="key"> <code>{{ child.name }}: {{ child.value }}</code> </li> 
+              </ul> 
+            </template> 
+            </DropDown> 
+          </div> 
+          <div v-else-if="tshark.value"> 
+            <li>{{ tshark.name }}: {{ tshark.value }}</li> 
+          </div> 
+          </div> 
+        </ul> 
+        </v-card-text> </code> 
+      </template> 
+    </DropDown> 
+    </v-card> 
+  </div> 
+  <div v-else>
+    There was an error while loading data 
+  </div> 
+  </div> 
 </template>
 
 <script>
@@ -78,7 +74,11 @@ export default {
         {name: 'Source', children: [...bits.slice(2, 4)]}, // {name: 'Address', value: addresses[1]}
         {name: 'Type'}, 
         {name: 'Frame check sequence'}, 
-        {name: 'FCS Status'}
+        {name: 'FCS Status'},
+        {name: 'Length'},
+        {name: 'Expert Info*'}, // Asteriks means key is not static
+        {name: 'Severity level'},
+        {name: 'Group'}
       ]
       return ord
     },
@@ -99,7 +99,6 @@ export default {
       return ord
     },
     UDP() {
-      //console.log(obj)
       let ord = [
         {name: 'Source Port'},
         {name: 'Destination Port'},
@@ -107,7 +106,7 @@ export default {
         {name: 'Checksum', children: [{name: 'Calculated Checksum'}]},
         {name: 'Checksum Status'},
         {name: 'Stream index'},
-        {name: 'Timestamps', children: [ // For some reason this is a value???
+        {name: 'Timestamps', children: [ 
           {name: 'Time since first frame'}, 
           {name: 'Time since previous frame'}]
         },
@@ -182,7 +181,7 @@ export default {
         {name: 'Window'},
         {name: 'Calculated window size'},
         {name: 'Checksum', children: [
-            {name: 'Calculated Cheksum'}
+            {name: 'Calculated Checksum'}
         ]},
         {name: 'Checksum Status'},
         {name: 'Urgent Pointer'},
@@ -192,57 +191,194 @@ export default {
         ]},
       ]
       return ord
+    },
+    DNS(bits) {
+      let ord = [
+        {name: 'Transaction ID'},
+        {name: 'Flags', children: [...bits]},
+        {name: 'Questions'},
+        {name: 'Answer RRs'},
+        {name: 'Authority RRs'},
+        {name: 'Additional RRs'},
+        {name: 'Queries'}
+      ]
+      return ord
+    },
+    PTP(bits) {
+      let ord = [
+        ...bits.slice(0, 5),
+        {name: 'messageLength'},
+        {name: 'subdomainNumber'},
+        {name: 'Reserved'},
+        {name: 'flags', children: [
+          ...bits.slice(5)
+        ]},
+        {name: 'correction', children: [
+          {name: 'correction: Ns'},
+          {name: 'correctionSubNs'}
+        ]},
+        {name: 'Reserved'},
+        {name: 'ClockIdentity'},
+        {name: 'SourcePortID'},
+        {name: 'sequenceId'},
+        {name: 'control'},
+        {name: 'logMessagePeriod'},
+        {name: 'originTimestamp (seconds)'},
+        {name: 'originTimestamp (nanoseconds)'}
+      ]
+      return ord
+    },
+    TELNET(bits, length) {
+      let dataChildren = []
+      for(let i = 0; i < length; i++)
+        dataChildren.push({name: 'Data-' + i})
+
+      let ord = [
+        {name: 'Data', children: [...dataChildren]}
+      ]
+      return ord
+    },
+    ICMPV6(bits, length) {
+      let ord = [
+        {name: 'Type'},
+        {name: 'Code'},
+        {name: 'Checksum'},
+        {name: 'Checksum Status'},
+        {name: 'Reserved'},
+        {name: 'Target Address'},
+        {name: 'ICMPv6 Option*', children: [
+          {name: 'Type-0'},
+          {name: 'Length'},
+          {name: 'Link-layer address'},
+          {name: 'Source Link-layer address'}
+        ]},
+      ]
+      return ord
+    },
+    IGMP() {
+      let ord = [
+        {name: 'IGMP Version'},
+        {name: 'Type'},
+        {name: 'Max Resp Time'},
+        {name: 'Checksum'},
+        {name: 'Checksum Status'},
+        {name: 'Multicast Address'}
+      ]
+      return ord
+    },
+    WINSREPL() {
+      let ord = [
+        {name: 'Packet Size'},
+        {name: 'Opcode'},
+        {name: 'Assoc_Ctx'},
+        {name: 'Message_Type'},
+        {name: 'WREPL_START_ASSOCIATION', children: [
+          {name: 'Assoc_Ctx'},
+          {name: 'Minor Version'},
+          {name: 'Major Version'}
+        ]}
+      ]
+      return ord
+    },
+    TPKT() {
+      let ord = [
+        {name: 'Version'},
+        {name: 'Reserved'},
+        {name: 'Length'}
+      ]
+      return ord
     }
   },
   mounted() {
     let obj = {}
     let bits = []
     let ord = []
+    let index = 0
+    let repeatingItems = 0
+    let copy = this.data.tshark_raw_summary.slice()
 
+    // Converts tshark data from list to an object
     this.data.tshark_raw_summary.forEach(element => {
-      let key = element.substring(0, element.indexOf(":"))
+      let key = element.substring(0, element.indexOf(":")).trim()
       let value = element.substring(element.indexOf(":") + 1).trim()
-      // There are some annoying corner cases where
-      // key is inside value
-      if(!key){
-        key = value
-        //if(key.includes("(")) 
-        //  key = key.substring(0, value.indexOf("("))
-        //value = value.substring(value.indexOf("(") + 1, value.indexOf(")")).trim()
+      // Repeating keys really?????
+      // I've spent at least 300 of my IQ on this one
+      // and it still doesn't work great
+      if(key in obj && this.data.tshark_name !== 'ETH'){
+        repeatingItems = this.data.tshark_raw_summary.filter((item) => item.match(key + ': ')).length 
+        index = repeatingItems - copy.filter((item) => item.match(key)).length
+        copy.splice(copy.indexOf(copy.filter(x => x.match(key + ':'))[0]), 1)
+        //console.log('[DEBUG]: ' + element + ' '+ index)
+        key = key + '-' + index
+      }
+      // Who named keys 'correction:' and 'correction: Ns:'??!?!?!?!
+      if(element.split('').filter(x => x === ":").length === 2){
+        key = element.substring(0, element.lastIndexOf(":")).trim()
+        value = element.substring(element.lastIndexOf(":") + 1).trim()
       }
 
+      // There are some annoying corner cases where
+      // key is inside value. This handles them
+      if(!key){
+        key = value.trim()
+        if(key.includes("(")) {
+          key = key.substring(0, value.indexOf("(")).trim()
+          value = value.substring(value.indexOf("(") + 1, value.indexOf(")")).trim()
+        }
+        // For some reason children don't render properly
+        // when parent element has no value
+        else value = " "
+      }
+
+      // Takes care of bits and flags
       else if(key.includes(' =')) {
         let bit = {}
         bit.name = key
         bit.value = value
         bits.push(bit)
       }
-      else obj[key] = value
+      obj[key] = value
     })
-    if(this.data.tshark_name == 'TCP')
-      console.log(this.data.tshark_raw_summary)
-    ord = this[this.data.tshark_name](bits)
+
+    //console.log(this.data.tshark_raw_summary)
+    console.log(obj)
+    // Exectues function that match tshark's name
+    ord = this[this.data.tshark_name](bits, repeatingItems - 1)
 
     for(const attr of ord){
-      if(obj[attr.name]){
+      // Handles non-static keys
+      if(attr.name.includes('*')){
+        const name = attr.name.slice(0, attr.name.indexOf('*'))
+        let index = Object.keys(obj).findIndex((el) => {
+          return el.match(name)
+        })
+        attr.name = Object.keys(obj)[index]
+        attr.value = ord[Object.keys(obj)[index]]
+      }
+
+      // Assigns value to items and its children
+      if(obj[attr.name])
         attr.value = obj[attr.name]
         if(attr.children)
           for(const child of attr.children)
-            if(child.name in obj) 
+            if(obj[child.name]) 
               child.value = obj[child.name]
-      }
     }
-    //console.log(ord)
+    //console.log(obj)
     this.sortedData = ord
   }
 }
 </script>
 
 <style>
-.dropdown-title {
+.DropDown-title {
   cursor: pointer;
 }
 .collapse {
-  /*list-style-type: none;*/
+  list-style-type: none;
+  margin-left: -15px;
+}
+.collapse::before {
+  content: '\27A4 '
 }
 </style>
