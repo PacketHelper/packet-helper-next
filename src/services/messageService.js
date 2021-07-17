d7958d68180f1735531dcd3bde25faa75922d5b2
Nex Sabre <NexSabre@users.noreply.github.com>
2021-07-17 16:10:32 +0200

    Remove unused elements from template

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -1,18 +1,6 @@
 import api from '@/services/api'
 
 export default {
-    fetchMessages() {
-        return api.get(`messages/`)
-            .then(response => response.data)
-    },
-    postMessage(payload) {
-        return api.post(`messages/`, payload)
-            .then(response => response.data)
-    },
-    deleteMessage(msgId) {
-        return api.delete(`messages/${msgId}`)
-            .then(response => response.data)
-    },
     getHex(hex_string) {
         return api.get(`hex/${hex_string}`)
             .then(response => response.data);
