9218ee4209c3eb8d115ee37d0a0cd57cf3d2a196
Gruzjan <gracjan122@protonmail.com>
2021-07-20 16:08:51 +0200

    Minor change

--- backend/urls.py
+++ backend/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     path("api/info", InfoViewSet.as_view()),
     path("api/create", CreateViewSet.as_view()),
     path("api/hex/<str:hex_string>/like", Hex2ViewSet.as_view(), like),
-    path("api/hex/<str:hex_string>/unlike", Hex2ViewSet.as_view(), dislike),
+    path("api/hex/<str:hex_string>/dislike", Hex2ViewSet.as_view(), dislike),
 ]
