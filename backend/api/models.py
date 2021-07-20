902a4351042322a38802220bebe76d0a5095e05f
Gruzjan <gracjan122@protonmail.com>
2021-07-20 16:07:01 +0200

    Add like/dislike functionality

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class Hexes(models.Model):
     hex = models.TextField()
-    like = models.PositiveIntegerField(default=0)
-    unlike = models.PositiveIntegerField(default=0)
+    likes = models.PositiveIntegerField(default=0)
+    dislikes = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return (
             self.hex[:10]
-            + "... Likes|Unlikes: "
-            + str(self.like)
+            + "... Likes|Dislikes: "
+            + str(self.likes)
             + "|"
-            + str(self.unlike)
+            + str(self.dislikes)
         )
--- backend/urls.py
+++ backend/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 
-<<<<<<< HEAD
 from .api.views import index_view
 from .api.api_views.create_view_set import CreateViewSet
 from .api.api_views.info_view_set import InfoViewSet
 from .api.api_views.hex2_view_set import Hex2ViewSet
-=======
-from .api.views import index_view, Hex2ViewSet, InfoViewSet, addHex
->>>>>>> Add django db model & add new hexes to it
+from .api.api_views.dislike_view_set import DislikeViewSet
+from .api.api_views.like_view_set import LikeViewSet
+from .api.api_views.add_hex_view_set import AddHexViewSet
 
 handler404 = "backend.api.views.handler404_redirect"
 
@@ -15,7 +14,11 @@ urlpatterns = [
     # landing page
     path("", index_view, name="index"),
     # API's
-    path("api/hex/<str:hex_string>", Hex2ViewSet.as_view(), addHex),
+    path("api/hex/<str:hex_string>", Hex2ViewSet.as_view()),
+    path("api/hex/<str:hex_string>/like", LikeViewSet.as_view()),
+    path("api/hex/<str:hex_string>/dislike", DislikeViewSet.as_view()),
+    path("api/add", AddHexViewSet.as_view()),
     path("api/info", InfoViewSet.as_view()),
+    path("api/packets", ScapyViewSet.as_view()),
     path("api/create", CreateViewSet.as_view()),
 ]
