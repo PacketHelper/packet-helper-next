9f013eec46337133b8828c69998d7c911b84fc06
Gruzjan <gracjan122@protonmail.com>
2021-07-20 08:44:37 +0200

    reformat with black

--- backend/api/migrations/0003_auto_20210719_1341.py
+++ backend/api/migrations/0003_auto_20210719_1341.py
@@ -6,18 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0002_auto_20210719_1243'),
+        ("api", "0002_auto_20210719_1243"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='hexes',
-            name='like',
+            model_name="hexes",
+            name="like",
             field=models.PositiveIntegerField(default=0),
         ),
         migrations.AlterField(
-            model_name='hexes',
-            name='unlike',
+            model_name="hexes",
+            name="unlike",
             field=models.PositiveIntegerField(default=0),
         ),
     ]
