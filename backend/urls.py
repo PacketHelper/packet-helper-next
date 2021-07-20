9fb4572e629e1e1529b294ceb6675e9377dfc723
Gruzjan <gracjan122@protonmail.com>
2021-07-20 08:29:47 +0200

    Add django db model & add new hexes to it

--- backend/urls.py
+++ backend/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     # landing page
     path("", index_view, name="index"),
     # API's
-    path("api/hex/<str:hex_string>", Hex2ViewSet.as_view()),
+    path("api/hex/<str:hex_string>", Hex2ViewSet.as_view(), addHex),
     path("api/info", InfoViewSet.as_view()),
     path("api/create", CreateViewSet.as_view()),
 ]
