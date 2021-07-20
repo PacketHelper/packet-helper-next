d8c3c9d2650375497a72e985c5b448ac9c219ad8
Gruzjan <gracjan122@protonmail.com>
2021-07-20 08:29:47 +0200

    Add django db model & add new hexes to it

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import redirect
 from django.views.decorators.cache import never_cache
 from django.views.generic import TemplateView
+<<<<<<< HEAD
+=======
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from scapy_helper import hexdump
+from backend.api.models import Hexes
+
+from packet_helper_core.packet_data import PacketData
+from packet_helper_core.packet_data_scapy import PacketDataScapy
+from packet_helper_core.utils.utils import decode_hex
+>>>>>>> Add django db model & add new hexes to it
 
 # Serve Vue Application
 index_view = never_cache(TemplateView.as_view(template_name="index.html"))
--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
+<<<<<<< HEAD
 from .api.views import index_view
 from .api.api_views.create_view_set import CreateViewSet
 from .api.api_views.info_view_set import InfoViewSet
 from .api.api_views.hex2_view_set import Hex2ViewSet
+=======
+from .api.views import index_view, Hex2ViewSet, InfoViewSet, addHex
+>>>>>>> Add django db model & add new hexes to it
 
 handler404 = "backend.api.views.handler404_redirect"
 
@@ -11,7 +15,7 @@ urlpatterns = [
     # landing page
     path("", index_view, name="index"),
     # API's
-    path("api/hex/<str:hex_string>", Hex2ViewSet.as_view()),
+    path("api/hex/<str:hex_string>", Hex2ViewSet.as_view(), addHex),
     path("api/info", InfoViewSet.as_view()),
     path("api/create", CreateViewSet.as_view()),
 ]
