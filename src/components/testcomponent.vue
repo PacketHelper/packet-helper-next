eca8ef546b0a60934afc45eb71176b62a6941bad
m1k53r <mikobw369@protonmail.com>
2021-07-21 19:45:19 +0200

    fix: fix commitlint

---
+++ src/components/testcomponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>test</h1>
+    <h2>test2</h2>
+    <h2>test2</h2>
+    <h2>test2</h2>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
