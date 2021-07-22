e970320a69860e5540d7a6232d957c009e01ab0f
Mikołaj Szustakowski <55512451+m1k53r@users.noreply.github.com>
2021-07-22 10:32:17 +0200

    Versioning (#24)
    * ci(github actions): add versioning
    
    (cherry picked from commit b00cbb994ab307ae976b717bd726484da90824b8)
    
    * Resolve merge conflicts
    
    * Resolve merge conflicts
    
    * Add husky hooks
    
    * fix: fix commitlint
    
    * test: remove test component
    
    * chore: rename release.yml
    
    * fix: fix dev dependencies
    
    * chore: minor change
    
    * chore: remove VERSION file
    
    * docs(contribute.md): add file to help new contributors

--- backend/api/views.py
+++ backend/api/views.py
@@ -52,11 +52,21 @@ class Hex2ViewSet(APIView):
 
 class InfoViewSet(APIView):
     def get(self, request, format=None):
-        with open("VERSION") as version_file:
-            version = version_file.read()
-
         from os import getenv
+        import requests
 
+        # Personal Access Token is needed
+        # as long as repo is private
+        token = getenv("PAT_TOKEN", False)
         revision = getenv("PH_REVISION", "dev")
+        if not token:
+            return Response({"version": "unknown", "revision": revision})
+        version = "unknown"
+        res = requests.get(
+            "https://api.github.com/repos/PacketHelper/packet-helper-next/releases/latest",
+            headers={"Authorization": f"token {token}"},
+        )
+        if res.ok:
+            version = res.json()["name"]
 
         return Response({"version": version, "revision": revision})
