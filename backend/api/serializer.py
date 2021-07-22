2b3ac5f4cb513422084d6b61179e7588609fff57
m1k53r <mikobw369@protonmail.com>
2021-07-22 20:51:54 +0200

    chore: remove file added by accident

--- backend/api/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import Hexes
-
-class HexSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Hexes
-        fields = ('Hex', 'Like', 'Dislike')
