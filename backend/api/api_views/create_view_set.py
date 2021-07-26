4e72e7ac38ce61325433f6f8ff88d05c0b4ea188
Nex Sabre <NexSabre@users.noreply.github.com>
2021-07-26 19:09:28 +0200

    refactor: Move all ViewSets to new files

---
+++ backend/api/api_views/create_view_set.py
@@ -0,0 +1,14 @@
+from http.client import CREATED
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from scapy_helper import get_hex
+
+from packet_helper_core.utils.conversion import from_sh_list
+
+
+class CreateViewSet(APIView):
+    def post(self, request, format=None):
+        return Response(
+            {"hex": get_hex(from_sh_list(request.data["json"]))}, status=CREATED
+        )
