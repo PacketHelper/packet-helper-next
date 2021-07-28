2986de2bc0944ffec38aa55168c9242a7ed6b3cf
m1k53r <mikobw369@protonmail.com>
2021-07-28 17:48:41 +0200

    chore(creator): progress save

--- src/components/Creator.vue
+++ src/components/Creator.vue
@@ -1,86 +1,6 @@
 <template>
   <div>
-    <v-card style="text-align: center">
-      <p>I want to create a/an...</p>
-      <v-text-field
-        label="Search"
-        prepend-icon="mdi-magnify"
-        v-model="filter"
-      ></v-text-field>
-      <v-virtual-scroll
-        height="100"
-        :item-height="50"
-        :items="filtered_packets"
-      >
-        <template v-slot:default="{ item }">
-          <v-chip
-            v-for="(packet, index) in item"
-            :key="index"
-            style="margin-left: 10px; margin-top: 10px"
-            @click="handleAdd"
-            :class="{ blue: pickedPackets.includes(packet) }"
-          >
-            {{ packet }}
-          </v-chip>
-        </template>
-      </v-virtual-scroll>
-      <v-btn @click="get_packets" color="orange">CREATE</v-btn>
-    </v-card>
-    <v-card style="margin-top: 1rem" v-if="pickedPackets.length">
-      <v-stepper v-model="stepper">
-        <v-stepper-header
-          ><v-stepper-step
-            v-for="(protocol, index) in pickedPackets"
-            :key="index"
-            :step="index"
-            @click="stepper = index"
-            style="cursor: pointer"
-          >
-            {{ protocol }}
-          </v-stepper-step>
-        </v-stepper-header>
-        <v-stepper-items>
-          <v-stepper-content
-            v-for="(input, protocolId) in inputs"
-            :key="protocolId"
-            :step="protocolId"
-          >
-            <v-form>
-              <v-container>
-                <v-row>
-                  <v-col cols="12" class="flex">
-                    <v-text-field
-                      v-for="(text, index) in input"
-                      :key="index"
-                      :label="text"
-                      :value="res[protocolId][pickedPackets[protocolId]][text]"
-                      v-model="res[protocolId][pickedPackets[protocolId]][text]"
-                      class="textInput"
-                      width="30px"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-form>
-            <v-btn @click="stepper--" v-if="stepper > 0">Previous</v-btn>
-            <v-btn
-              style="float: right"
-              @click="stepper++"
-              v-if="stepper < inputs.length - 1"
-              >Next</v-btn
-            >
-            <v-btn style="float: right" v-else @click="handleCreate"
-              >Create packet</v-btn
-            >
-          </v-stepper-content>
-        </v-stepper-items>
-      </v-stepper>
-    </v-card>
-    <v-card
-      style="padding: 10px; margin-top: 1rem"
-      elevation="6"
-      v-if="createdHex"
-    >
+    <v-card style="padding: 10px; margin-top: 1rem" elevation="6">
       <v-card-title>Your packet</v-card-title>
       <v-textarea
         name="input-7-4"
@@ -91,15 +11,184 @@
         label="Hexdump"
         elevation="6"
       ></v-textarea>
-      <v-btn class="button" color="blue">Copy</v-btn>
-      <v-btn v-if="upperCase" color="indigo" @click="changeCasing"
-        >To lower case</v-btn
-      >
-      <v-btn v-else color="indigo" @click="changeCasing">To upper case</v-btn>
-      <v-btn v-if="spacing" color="orange" @click="changeSpacing"
-        >Remove spacing</v-btn
-      >
-      <v-btn v-else color="orange" @click="changeSpacing">Add spacing</v-btn>
+      <div class="actionButtons">
+        <v-btn @click="createdHex = null" color="red" class="white--text"
+          ><v-icon>mdi-cancel</v-icon>Clear</v-btn
+        >
+        <v-btn class="button white--text" color="blue" @click="copy"
+          ><v-icon>mdi-content-copy</v-icon>Copy</v-btn
+        >
+        <v-btn
+          v-if="upperCase"
+          color="indigo"
+          @click="changeCasing"
+          class="white--text actionButton"
+          ><v-icon>mdi-arrow-down</v-icon>To lower case</v-btn
+        >
+        <v-btn
+          v-else
+          color="indigo"
+          @click="changeCasing"
+          class="white--text actionButton"
+          ><v-icon>mdi-arrow-up</v-icon>To upper case</v-btn
+        >
+        <v-btn
+          v-if="spacing"
+          color="orange"
+          @click="changeSpacing"
+          class="white--text actionButton"
+          ><v-icon>mdi-window-close</v-icon>Remove spacing</v-btn
+        >
+        <v-btn
+          v-else
+          color="orange"
+          @click="changeSpacing"
+          class="white--text actionButton"
+          ><v-icon>mdi-plus</v-icon>Add spacing</v-btn
+        >
+      </div>
+    </v-card>
+
+    <v-card>
+      <v-stepper v-model="stepper" height="">
+        <v-stepper-header>
+          <v-stepper-step step="0" @click="stepper = 0" style="cursor: pointer">
+            Choose packets
+          </v-stepper-step>
+          <div v-for="(protocol, index) in displayPackets" :key="index">
+            <v-stepper-step
+              :step="index + 1"
+              @click="stepper = index + 1"
+              style="cursor: pointer"
+            >
+              {{ protocol }}
+            </v-stepper-step>
+          </div>
+        </v-stepper-header>
+        <v-stepper-items>
+          <v-stepper-content step="0">
+            <div class="topBar">
+              <p style="padding-right: 5px">I want to create a/an...</p>
+              <div style="display: flex">
+                <div
+                  v-for="(pickedPacket, index) in pickedPackets"
+                  :key="index"
+                  style="display: flex"
+                  @click="handleAdd"
+                >
+                  {{ pickedPacket }}
+                  <div
+                    v-if="index != pickedPackets.length"
+                    style="margin: 0 5px 0 5px"
+                  >
+                    /
+                  </div>
+                </div>
+              </div>
+              <v-btn
+                style="margin-left: auto"
+                @click="get_packets"
+                color="orange"
+                class="white--text"
+                :disabled="!pickedPackets.length"
+                >Next <v-icon>mdi-arrow-right</v-icon></v-btn
+              >
+            </div>
+            <div class="searchInput">
+              <v-text-field
+                label="Search"
+                prepend-icon="mdi-magnify"
+                v-model="filter"
+              ></v-text-field>
+              <v-icon
+                style="position: absolute; right: 1.3rem; top: 5.5rem"
+                dense
+                @click="filter = ''"
+                >mdi-window-close</v-icon
+              >
+            </div>
+            <v-virtual-scroll
+              height="100"
+              :item-height="50"
+              :items="filtered_packets"
+            >
+              <template v-slot:default="{ item }">
+                <v-chip
+                  v-for="(packet, index) in item"
+                  :key="index"
+                  style="margin-left: 10px; margin-top: 10px"
+                  @click="handleAdd"
+                  :class="{ blue: pickedPackets.includes(packet.name) }"
+                >
+                  {{ packet.name }}
+                </v-chip>
+              </template>
+            </v-virtual-scroll>
+          </v-stepper-content>
+          <v-stepper-content
+            v-for="(input, protocolId) in inputs"
+            :key="protocolId"
+            :step="protocolId + 1"
+          >
+            <v-btn
+              @click="stepper--"
+              v-if="stepper > 0"
+              color="orange"
+              class="white--text"
+              ><v-icon>mdi-arrow-left</v-icon>Back
+            </v-btn>
+            <v-btn
+              style="float: right"
+              @click="stepper++"
+              v-if="stepper < inputs.length"
+              color="orange"
+              class="white--text"
+              >Next <v-icon>mdi-arrow-right</v-icon></v-btn
+            >
+            <v-btn
+              v-else
+              style="float: right"
+              @click="handleCreate"
+              color="green"
+              class="white--text"
+              >Create packet<v-icon>mdi-package-variant-closed</v-icon></v-btn
+            >
+            <v-form>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" class="flex">
+                    <v-text-field
+                      v-for="(text, index) in input"
+                      :key="index"
+                      :label="text"
+                      :value="
+                        res[protocolId][
+                          packets[
+                            packets.findIndex(
+                              (x) => x.name === displayPackets[protocolId]
+                            )
+                          ].scapy_name
+                        ][text]
+                      "
+                      v-model="
+                        res[protocolId][
+                          packets[
+                            packets.findIndex(
+                              (x) => x.name === displayPackets[protocolId]
+                            )
+                          ].scapy_name
+                        ][text]
+                      "
+                      class="textInput"
+                      width="30px"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-form>
+          </v-stepper-content>
+        </v-stepper-items>
+      </v-stepper>
     </v-card>
   </div>
 </template>
@@ -109,12 +198,41 @@ import ApiService from "../services/apiService.js";
 export default {
   data() {
     return {
-      packets: [["TCP", "IP", "DNS", "UDP", "GRE", "IPv6", "PPTP", "Ether"]],
+      packets: [
+        { name: "Ether", scapy_name: "Ethernet" },
+        { name: "IP", scapy_name: "IP" },
+        { name: "UDP", scapy_name: "UDP" },
+        { name: "TCP", scapy_name: "TCP" },
+        { name: "DNS", scapy_name: "DNS" },
+        { name: "GRE", scapy_name: "GRE" },
+        { name: "IPv6", scapy_name: "IPv6" },
+        { name: "PPTP", scapy_name: "PPTP" },
+        { name: "DHCP", scapy_name: "DHCP options" },
+        { name: "ICMP", scapy_name: "ICMP" },
+        { name: "Raw", scapy_name: "Raw" },
+        { name: "ARP", scapy_name: "ARP" },
+        { name: "VXLAN", scapy_name: "VXLAN" },
+        { name: "Padding", scapy_name: "Padding" },
+        { name: "PadN", scapy_name: "PadN" },
+        { name: "Pad1", scapy_name: "Pad1" },
+        { name: "MGCP", scapy_name: "MGCP" },
+        { name: "Loopback", scapy_name: "Loopback" },
+        { name: "LLTD", scapy_name: "LLTD" },
+        { name: "LLC", scapy_name: "LLC" },
+        { name: "ISAKMP", scapy_name: "ISAKMP" },
+        { name: "HSRP", scapy_name: "HSRP" },
+        { name: "ESP", scapy_name: "ESP" },
+        { name: "EAP", scapy_name: "EAP" },
+        { name: "EAPOL", scapy_name: "EAPOL" },
+        { name: "BOOTP", scapy_name: "BOOTP" },
+        { name: "AH", scapy_name: "AH" },
+      ],
       packet: null,
       pickedPackets: [],
+      displayPackets: [],
       res: null,
       inputs: [],
-      stepper: null,
+      stepper: 0,
       filter: "",
       createdHex: null,
       upperCase: false,
@@ -124,24 +242,27 @@ export default {
   computed: {
     filtered_packets() {
       return [
-        this.packets[0].filter((x) =>
-          x.toLowerCase().includes(this.filter.toLowerCase())
+        this.packets.filter((x) =>
+          x.name.toLowerCase().includes(this.filter.toLowerCase())
         ),
       ];
     },
   },
   methods: {
+    delay(seconds) {
+      return new Promise((res) => setTimeout(res, seconds * 1000));
+    },
     async get_packets() {
-      this.createdHex = null;
       this.res = await ApiService.getScapy(this.pickedPackets);
+      this.displayPackets = [];
       this.inputs = [];
       for (let i = 0; i < this.res.length; i++) {
         let temp_packet = Object.keys(this.res[i]);
-        console.log(temp_packet);
         this.inputs.push(Object.keys(this.res[i][temp_packet]));
       }
-      console.log(this.res[0][Object.keys(this.res[0])]);
-      this.stepper = -1;
+      this.displayPackets = this.pickedPackets.slice();
+      await this.delay(0.5);
+      this.stepper++;
       /*
       this.steps = [...Array(this.inputs.length).keys()].filter(
         (n) => !(n % 3)
@@ -163,9 +284,12 @@ export default {
     },
     handleAdd(el) {
       const protocol = el.srcElement.innerText;
-      if (!this.pickedPackets.includes(protocol)) {
+      if (
+        !this.pickedPackets.includes(protocol) &&
+        this.pickedPackets.length < 4
+      ) {
         this.pickedPackets.push(protocol);
-      } else {
+      } else if (this.pickedPackets.includes(protocol)) {
         this.pickedPackets.splice(this.pickedPackets.indexOf(protocol), 1);
       }
     },
@@ -190,6 +314,9 @@ export default {
         this.spacing = true;
       }
     },
+    async copy() {
+      await navigator.clipboard.writeText(this.createdHex);
+    },
   },
 };
 </script>
@@ -209,5 +336,14 @@ export default {
 }
 .button {
   float: right;
+}
+.topBar {
+  display: flex;
+}
+.actionButton {
+  margin-left: 3px;
+}
+.searchInput {
+  display: flex;
 }
 </style>
