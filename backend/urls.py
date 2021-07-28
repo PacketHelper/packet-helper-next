0e5e1a351259c348fcedb354eb55c11a39aac266
Gruzjan <gracjan122@protonmail.com>
2021-07-28 18:40:23 +0200

    fix: adding hex now from different endpoint

--- backend/urls.py
+++ backend/urls.py
@@ -6,6 +6,7 @@ from .api.api_views.info_view_set import InfoViewSet
 from .api.api_views.hex2_view_set import Hex2ViewSet
 from .api.api_views.dislike_view_set import DislikeViewSet
 from .api.api_views.like_view_set import LikeViewSet
+from .api.api_views.add_hex_view_set import AddHexViewSet
 
 handler404 = "backend.api.views.handler404_redirect"
 
@@ -14,8 +15,9 @@ urlpatterns = [
     path("", index_view, name="index"),
     # API's
     path("api/hex/<str:hex_string>", Hex2ViewSet.as_view()),
-    path("api/hex/<str:hex_string>/like", LikeViewSet.as_view(), name="like"),
-    path("api/hex/<str:hex_string>/dislike", DislikeViewSet.as_view(), name="dislike"),
+    path("api/hex/<str:hex_string>/like", LikeViewSet.as_view()),
+    path("api/hex/<str:hex_string>/dislike", DislikeViewSet.as_view()),
+    path("api/add", AddHexViewSet.as_view()),
     path("api/info", InfoViewSet.as_view()),
     path("api/create", CreateViewSet.as_view()),
 ]
