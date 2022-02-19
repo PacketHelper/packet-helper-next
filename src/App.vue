37997697b910515b6ba80889559761211134169d
Nex Sabre <NexSabre@users.noreply.github.com>
2022-02-19 22:50:55 +0100

    Disable creator

--- src/App.vue
+++ src/App.vue
@@ -27,20 +27,16 @@
               ></router-link
             >
             <!-- <v-btn text disabled>Visual</v-btn> -->
-            <router-link to="/creator"
+            <!-- <router-link to="/creator"
               ><v-btn text
                 ><v-badge content="Preview">Creator</v-badge></v-btn
               ></router-link
-            >
+            > -->
             <v-spacer></v-spacer>
-            <v-btn text disabled
-              >"Craft packets before, packets craft you" 🐱‍👤
-            </v-btn>
+            <v-btn text disabled>"Craft packets before, packets craft you" 🐱‍👤 </v-btn>
             <v-dialog v-model="dialog" width="500">
               <template v-slot:activator="{ on, attrs }">
-                <v-icon color="grey" v-bind="attrs" v-on="on">
-                  mdi-help-circle
-                </v-icon>
+                <v-icon color="grey" v-bind="attrs" v-on="on"> mdi-help-circle </v-icon>
               </template>
 
               <v-card>
@@ -49,11 +45,10 @@
                   "Craft packets before, packets craft you" 🐱‍👤
                 </v-card-subtitle>
                 <v-card-text>
-                  The application has been prepared to facilitate the work of
-                  all those who work with internet packages on a daily basis. If
-                  you program in P4 Lang at your work, use the PTF framework or
-                  you don't know what happened to your package during the
-                  laboratories at the university.
+                  The application has been prepared to facilitate the work of all those
+                  who work with internet packages on a daily basis. If you program in P4
+                  Lang at your work, use the PTF framework or you don't know what
+                  happened to your package during the laboratories at the university.
                   <br /><br />Find us at:<br />
                   <a href="https://github.com/PacketHelper/packet-helper-next"
                     ><v-icon>mdi-github</v-icon> GitHub
@@ -66,9 +61,7 @@
                   <v-btn text :href="this.getLinkToLatestRelease()">
                     Latest version {{ this.version }}
                   </v-btn>
-                  <v-btn color="primary" text @click="dialog = false">
-                    Close
-                  </v-btn>
+                  <v-btn color="primary" text @click="dialog = false"> Close </v-btn>
                 </v-card-actions>
               </v-card>
             </v-dialog>
@@ -81,8 +74,7 @@
         <v-container fluid>
           <v-container>
             <v-alert icon="mdi-shield-lock-outline" prominent text type="info">
-              <strong>Legacy version</strong> of the PacketHelper is available
-              at
+              <strong>Legacy version</strong> of the PacketHelper is available at
               <a href="http://legacy.packethelper.com"
                 >http://legacy.packethelper.com
               </a>
@@ -99,58 +91,58 @@
 </template>
 
 <script>
-import ApiService from "@/services/apiService";
+  import ApiService from "@/services/apiService";
 
-export default {
-  name: "Info",
-  data() {
-    return {
-      version: 0,
-      revision: "dev",
-      dialog: false,
-    };
-  },
-  methods: {
-    async getInfo() {
-      const info = await ApiService.getInfo();
-      this.version = info["version"];
-      this.revision = info["revision"];
+  export default {
+    name: "Info",
+    data() {
+      return {
+        version: 0,
+        revision: "dev",
+        dialog: false,
+      };
     },
-    getLinkToLatestRelease() {
-      if (this.version === 0) {
-        return "https://github.com/PacketHelper/packet-helper-next";
-      }
-      return (
-        "https://github.com/PacketHelper/packet-helper-next/releases/tag/" +
-        this.version
-      );
+    methods: {
+      async getInfo() {
+        const info = await ApiService.getInfo();
+        this.version = info["version"];
+        this.revision = info["revision"];
+      },
+      getLinkToLatestRelease() {
+        if (this.version === 0) {
+          return "https://github.com/PacketHelper/packet-helper-next";
+        }
+        return (
+          "https://github.com/PacketHelper/packet-helper-next/releases/tag/" +
+          this.version
+        );
+      },
     },
-  },
-  mounted() {
-    this.getInfo();
-  },
-};
+    mounted() {
+      this.getInfo();
+    },
+  };
 </script>
 
 <style>
-#version {
-  color: grey;
-}
-a {
-  text-decoration: none;
-}
-.route-enter {
-  opacity: 0;
-  transform: translateX(-200px);
-}
-.route-enter-active {
-  transition: all 0.3s ease-out;
-}
-.route-leave-to {
-  opacity: 0;
-  transform: translateX(200px);
-}
-.route-leave-active {
-  transition: all 0.3s ease-in;
-}
+  #version {
+    color: grey;
+  }
+  a {
+    text-decoration: none;
+  }
+  .route-enter {
+    opacity: 0;
+    transform: translateX(-200px);
+  }
+  .route-enter-active {
+    transition: all 0.3s ease-out;
+  }
+  .route-leave-to {
+    opacity: 0;
+    transform: translateX(200px);
+  }
+  .route-leave-active {
+    transition: all 0.3s ease-in;
+  }
 </style>
