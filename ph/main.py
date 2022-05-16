789db7e24341f2403e4e5797021e7388f2a4a8fe
Nex Sabre <NexSabre@users.noreply.github.com>
2022-05-16 21:34:12 +0200

    Hide default endpoints in the swagger /docs

--- ph/main.py
+++ ph/main.py
@@ -21,19 +21,19 @@ app.include_router(api, prefix="/api", tags=["api"])
 templates = Jinja2Templates(directory="static")
 
 
-@app.get("/")
+@app.get("/", include_in_schema=False)
 def get_root(request: Request, status_code=status.HTTP_200_OK) -> HTMLResponse:
     """Return Vue singlepage"""
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.get("/hex/{hex_string}")
+@app.get("/hex/{hex_string}", include_in_schema=False)
 def get_hex(request: Request, status_code=status.HTTP_200_OK) -> HTMLResponse:
     """Return specific path for Vue singlepage"""
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.get("/version", status_code=status.HTTP_200_OK)
+@app.get("/version", status_code=status.HTTP_200_OK, include_in_schema=False, deprecated=True)
 def get_version() -> VersionResponse:
     """Return information about version of the Packethelper"""
     return VersionResponse(packethelper="0.1", framework="fastapi")
