9cad7a89140d470d71e557d8e45dec139f654f99
Nex Sabre <NexSabre@users.noreply.github.com>
2022-07-02 14:21:22 +0200

    feat: Open PacketHelper API

--- ph/main.py
+++ ph/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request, status
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -15,6 +16,13 @@ app = FastAPI(
         "url": "https://github.com/PacketHelper/packet-helper-next/blob/main/LICENSE",
     },
 )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(api, prefix="/api", tags=["api"])
 
@@ -22,13 +30,13 @@ templates = Jinja2Templates(directory="static")
 
 
 @app.get("/", include_in_schema=False)
-def get_root(request: Request, status_code=status.HTTP_200_OK) -> HTMLResponse:
+def get_root(request: Request, status_code=status.HTTP_200_OK):
     """Return Vue singlepage"""
     return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.get("/hex/{hex_string}", include_in_schema=False)
-def get_hex(request: Request, status_code=status.HTTP_200_OK) -> HTMLResponse:
+def get_hex(request: Request, status_code=status.HTTP_200_OK):
     """Return specific path for Vue singlepage"""
     return templates.TemplateResponse("index.html", {"request": request})
 
