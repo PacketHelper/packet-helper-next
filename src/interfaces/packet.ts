c491bec2f69ee2f8990f664b37544177afadf53f
Nex Sabre <NexSabre@users.noreply.github.com>
2022-07-26 16:43:03 +0200

    feat: Add highlight for frames position in the hexdump (#80)
    feat: Add highlight for frames position in the hexdump

--- src/interfaces/packet.ts
+++ src/interfaces/packet.ts
@@ -2,3 +2,22 @@ export default interface Packet {
   name: string;
   scapy_name: string;
 }
+
+export interface StructureChksumStatus {
+  chksum: string;
+  chksum_calculated: string;
+  status: boolean | null;
+}
+
+export interface Structure {
+  name: string;
+  bytes: string;
+  hex: string;
+  hex_one: string;
+  length: number;
+  repr: string;
+  repr_full: string;
+  tshark_name: string;
+  tshart_raw_summary: string[];
+  chksum_status: StructureChksumStatus;
+}
