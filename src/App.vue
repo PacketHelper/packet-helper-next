f191267f26fbf0370aec09f5ac8fd0c129414687
Nex Sabre <NexSabre@users.noreply.github.com>
2022-10-24 12:03:21 +0200

    fix: Remove information about legacy version and fix typo

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,6 @@
         <!-- Provides the application the proper gutter -->
         <v-container fluid>
           <v-container>
-            <v-alert icon="mdi-shield-lock-outline" prominent text type="info">
-              <strong>Legacy version</strong> of the PacketHelper is available
-              at
-              <a href="http://legacy.packethelper.com"
-                >http://legacy.packethelper.com
-              </a>
-            </v-alert>
             <!-- If using vue-router -->
             <transition name="route" mode="out-in">
               <router-view> </router-view>
